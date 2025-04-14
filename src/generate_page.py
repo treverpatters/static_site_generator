e353b918b3b536041dedad037885b73ae7eb6e0e
treverleach19 <treverleach19@gmail.com>
2025-04-14 08:53:00 -0500

    added basepath functionality, added public to commit in order to upload website to githubio pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -17,7 +17,7 @@ def extract_title(markdown):
     else:
         raise Exception("no h1 header")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as file:
         page = file.read()
@@ -31,12 +31,14 @@ def generate_page(from_path, template_path, dest_path):
     # Replace {{ Title }} and {{ Content }} placeholders in template with HTML and title
     added_title = template.replace("{{ Title }}", page_title)
     added_content = added_title.replace("{{ Content }}", page_html_string)
+    content = added_content.replace('href="/', f'href={basepath}')
+    content = content.replace('src="/', f'src="{basepath}')
     # Now write the new full HTML page to a file at dest_path. Be sure to create any necessary directories if they don't exist
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
-        f.write(added_content)
+        f.write(content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     # initiate variables
     for subdir, dirs, files in os.walk(dir_path_content):
         for file in files:
@@ -49,7 +51,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
             os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
 
-            generate_page(source_file_path, template_path, dest_file_path)
+            generate_page(source_file_path, template_path, dest_file_path, basepath)
             
 
     # crawl through content directory
--- src/main.py
+++ src/main.py
@@ -2,15 +2,19 @@ from textnode import *
 from tools import *
 from copy_static import *
 from generate_page import *
+import sys
 
 def main():
     
     copy_static()
-
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
     template_path = "template.html"
     content_dir = "content"
-    public_dir = "public"
-    generate_pages_recursive(content_dir, template_path, public_dir)
+    docs_dir = "docs"
+    generate_pages_recursive(content_dir, template_path, docs_dir, basepath)
 
 if __name__ == "__main__":
     main()
