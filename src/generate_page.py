6d828a9c25917f3d179d66bfcd86d03a4d08c1e9
treverleach19 <treverleach19@gmail.com>
2025-04-15 16:44:30 -0500

    this one.

--- src/generate_page.py
+++ src/generate_page.py
@@ -25,23 +25,28 @@ def generate_page(from_path, template_path, dest_path, basepath):
     with open(template_path, "r") as file:
         template = file.read()
     
-    basepath = basepath.rstrip("/") + "/"
+    
     page_html = markdown_to_html_node(page) 
     page_html_string = page_html.to_html()
     page_title = extract_title(page)
 
     # Replace {{ Title }} and {{ Content }} placeholders in template with HTML and title
-    added_title = template.replace("{{ Title }}", page_title)
-    added_content = added_title.replace("{{ Content }}", page_html_string)
-    content = re.sub(r'href="/', f'href="{basepath}', added_content)
-    content = re.sub(r'src="/', f'src="{basepath}', content)
-    content = re.sub(r'(<img[^>]*src=")(/[^"]*)(\")', f'\\1{basepath}\\2\\3', content)
-    content = re.sub(r'([^:])//+', r'\1/', content)
+    basepath = basepath.strip("/")
+    if basepath:
+        basepath = "/" + basepath + "/"
+    else:
+        basepath = "/"
+    
+    result = template.replace("{{ Title }}", page_title)
+    result = result.replace("{{ Content }}", page_html_string)
+
+    result = result.replace('href="/', f'href="{basepath}')
+    result = result.replace('src="/', f'src="{basepath}')
 
     # Now write the new full HTML page to a file at dest_path. Be sure to create any necessary directories if they don't exist
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
-        f.write(content)
+        f.write(result)
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     # initiate variables
